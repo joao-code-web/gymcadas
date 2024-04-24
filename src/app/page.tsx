0f672523bbd9cb6d47030c2cfe7681177ec79202
joao-code-web <joaogabriel9512@gmail.com>
2024-04-24 20:35:18 -0300

    joao

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,22 @@
 // Home.tsx
-"use client"
+"use client";
 
-import "./pag.css"
-
-import { useAllMonths } from "@/components/getMonths/getMonths"
+import "./pag.css";
+import { useAllMonths, getUnicMonthAll, getUsers } from "@/components/getMonths/getMonths";
 import axios from "axios";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import { MdDelete } from "react-icons/md";
 
 export default function Home() {
-
   const { allMonths, setAllMonths, fetGetMonth } = useAllMonths();
-
   const [formData, setFormData] = useState<string>("");
 
   const handleAddMonth = async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     e.preventDefault();
-
     if (!formData) {
       return;
     }
-
     try {
       const responseNewMonth = await axios.post(`http://localhost:8000/api/months/`, { Month: formData });
       console.log("Novo mês adicionado:", responseNewMonth.data);
@@ -35,38 +30,35 @@ export default function Home() {
   const handleDelete = async (id: string) => {
     try {
       if (!id) {
-        return
+        return;
       }
-
       const responseDelete = await axios.delete(`http://localhost:8000/api/months/?monthId=${id}`);
       console.log(responseDelete);
       fetGetMonth();
     } catch (error) {
-      console.log("erro")
+      console.log("erro");
     }
-  }
+  };
 
   useEffect(() => {
     fetGetMonth();
-  }, [fetGetMonth, setFormData])
+  }, [fetGetMonth]);
 
   return (
     <main>
-
       <form action="">
         <h1>Adicionar mês</h1>
         <input type="text" onChange={(e) => setFormData(e.target.value)} />
         <button onClick={handleAddMonth}>Adicionar</button>
       </form>
-
       <div className="content">
         {allMonths.map((monthsDads) => (
           <div className="month" key={`${monthsDads._id}`}>
-            <Link href={`monthsId/${monthsDads._id}`}>  <h1>{monthsDads.Month}</h1></Link>
+            <Link href={`monthsId/${monthsDads._id}`}> <h1>{monthsDads.Month}</h1></Link>
             <button onClick={() => handleDelete(String(monthsDads._id))}><MdDelete /></button>
           </div>
         ))}
       </div>
     </main>
-  )
+  );
 }
--- src/components/getMonths/getMonths.tsx
+++ src/components/getMonths/getMonths.tsx
@@ -2,23 +2,60 @@
 import axios from "axios";
 import { useState } from "react";
 
-interface typesMonth {
-    _id: string;
-    Month: string;
+interface MonthTypes {
+  _id: string;
+  Month: string;
 }
 
 export function useAllMonths() {
-    const [allMonths, setAllMonths] = useState<typesMonth[]>([]);
+  const [allMonths, setAllMonths] = useState<MonthTypes[]>([]);
 
-    const fetGetMonth = async () => {
-        try {
-            const response = await axios.get("http://localhost:8000/api/months");
-            const months = response.data;
-            setAllMonths(months);
-        } catch (error) {
-            console.log("deu error");
-        }
+  const fetGetMonth = async () => {
+    try {
+      const response = await axios.get("http://localhost:8000/api/months");
+      const months = response.data;
+      setAllMonths(months);
+    } catch (error) {
+      console.log("deu error");
     }
+  };
 
-    return { allMonths, setAllMonths, fetGetMonth }
+  return { allMonths, setAllMonths, fetGetMonth };
+}
+
+interface UsersTypes {
+  _id: string;
+  nameuser: string;
+  value: number;
+  dataPag: string;
+}
+
+export function getUsers() {
+  const [getUsersAll, setGetUsers] = useState<UsersTypes[]>([]);
+
+  const getUsersAllFetch = async (id: string) => {
+    try {
+      const response = await axios.get(`http://localhost:8000/api/users/?monthId=${id}`);
+      setGetUsers(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return { getUsersAll, getUsersAllFetch };
+}
+
+export function getUnicMonthAll() {
+  const [getUnicMonth, setGetUnicMonth] = useState<MonthTypes[]>([]);
+
+  const getUnicMonthFetch = async (id: string) => {
+    try {
+      const response = await axios.get(`http://localhost:8000/api/months/${id}`);
+      setGetUnicMonth([response.data]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return { getUnicMonth, getUnicMonthFetch };
 }
